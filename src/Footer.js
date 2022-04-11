8d089c9dcc2d4822a1fa5ad7a38dfd55cbf3b192
GinaCamelia <92101771+GinaCamelia@users.noreply.github.com>
2022-04-11 13:36:34 +0100

    Added link to my git repository and customised it

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,10 @@
 import React from "react";
 
+
 export default function Footer() {
     return (
-        <div className="footer">
-            <a href="https://github.com/GinaCamelia/weather-react">Open-source code</a>
+        <div className="Footer mt-5 fs-5 fw-lighter lh-lg text-decoration-none">
+            <a href="https://github.com/GinaCamelia/weather-react" target="_blank" rel="noreferrer">Open-source code</a>
         </div>
     );
 } 
