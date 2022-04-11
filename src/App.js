bd06cb2128de2fc34727abdb26a23e22809bbf1c
GinaCamelia <92101771+GinaCamelia@users.noreply.github.com>
2022-04-11 20:55:57 +0100

    Made styling changes to my footer

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Footer from "./Footer";
 
 export default function App() {
   return (
-    <div className="App text-center lh-lg mt-5">
+    <div className="App text-center lh-lg mt-5 mb-5">
         <h1>Hello from React</h1>
         <Weather city="Bucharest"/>
         <Footer />
--- src/Footer.js
+++ src/Footer.js
@@ -3,8 +3,9 @@ import React from "react";
 
 export default function Footer() {
     return (
-        <div className="Footer mt-5 fs-5 fw-lighter lh-lg text-decoration-none">
-            <a href="https://github.com/GinaCamelia/weather-react" target="_blank" rel="noreferrer">Open-source code</a>
+        <div className="Footer mt-5 fs-5 fw-lighter lh-lg">
+            Coded by <a href="https://laughing-kilby-b8cd7c.netlify.app" target="_blank" rel="noreferrer" class="links">Gina-Camelia Hodorogea</a> and it is{" "}
+            <a href="https://github.com/GinaCamelia/weather-react" target="_blank" rel="noreferrer" class="links">open-source code</a>
         </div>
     );
 } 
