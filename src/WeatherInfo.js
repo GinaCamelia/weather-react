7e92bb8a30294ea5a20ae8d74ba3586e5435eb01
GinaCamelia <92101771+GinaCamelia@users.noreply.github.com>
2022-04-16 20:15:51 +0100

    improved styling

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -10,32 +10,32 @@ export default function WeatherInfo(props) {
                 <div className="row mt-5 mb-5">
                   <div className="col-sm-6">
                     <span className="circle" id="display-input">
-                      <span className="day"></span> <br />
+                      <span className="day"></span>
                       <span>
                             <FormattedDate date={props.data.date} />
                         </span>
                       <span className="city">{props.data.city}</span> <br />
                       <WeatherTemperature celsius={props.data.temperature} />
                       <WeatherIcon code={props.data.icon} rel="noreferrer" />
-                      <span className="description text-capitalize">{props.data.description}</span>
+                      <span className="description text-capitalize mt-1">{props.data.description}</span>
                     </span>
                   </div>
                   <div className="col-sm-6">
                     <span className="square">
-                      <div className="col-sm mb-1">
-                      💨 Wind: <span className="wind">{Math.round(props.data.wind)} km/h</span>
+                      <div className="col-sm">
+                       Wind: <span className="wind">{Math.round(props.data.wind)} km/h</span>
                         <br />
                       </div>
-                        <div className="col-sm mb-1">
-                      🌪 Pressure: <span className="pressure">{Math.round(props.data.pressure)} mHg</span>
+                        <div className="col-sm">
+                        Pressure: <span className="pressure">{Math.round(props.data.pressure)}mHg</span>
                               <br />
                               </div>
-                      <div className="col-sm mb-1">
-                      💧 Humidity: <span className="humidity">{Math.round(props.data.humidity)} %</span>
+                      <div className="col-sm">
+                      Humidity: <span className="humidity">{Math.round(props.data.humidity)}%</span>
                       <br />
                       </div>
-                      <div className="col-sm mb-5">
-                     🌡 Feels-like: <span className="feels-like">{Math.round(props.data.MaxTemp)} ºC</span>
+                      <div className="col-sm">
+                     Max-Temp: <span className="feels-like">{Math.round(props.data.MaxTemp)}ºC</span>
                       </div> 
                     </span>
                   </div>
