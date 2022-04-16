a4085b6a967bde49fa4acb410ca71bb91ff74cce
GinaCamelia <92101771+GinaCamelia@users.noreply.github.com>
2022-04-16 15:52:46 +0100

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -4,20 +4,20 @@ import React from "react";
 export default function Footer() {
     return (
       <div className="Footer">
-        <div class="link-github">
+        <div className="link-github">
               <a href="https://github.com/GinaCamelia/my-awesome-weather-app" 
-              class="link"
+              className="link"
               target="_blank"
               rel="noreferrer">Open-source code{" "}</a>
               by <a href="https://laughing-kilby-b8cd7c.netlify.app" 
-              class="link"
+              className="link"
               target="_blank"
               rel="noreferrer">Gina-Camelia Hodorogea </a>
             </div>
-              <div class="contact-details">
-                <span class="email"><strong>E-mail:{" "}</strong></span>
+              <div className="contact-details">
+                <span className="email"><strong>E-mail:{" "}</strong></span>
                 camelia_hodorogea@yahoo.com
-                <span class="instagram"><strong>{" "}Instagram:</strong></span> @gina.camelia
+                <span className="instagram"><strong>{" "}Instagram:</strong></span> @gina.camelia
               </div>   
         </div>
     );
