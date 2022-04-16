51fee078b937589aa558b36e9f5c2631049dde1a
GinaCamelia <92101771+GinaCamelia@users.noreply.github.com>
2022-04-16 19:11:46 +0100

    Update WeatherIcon.js

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -26,7 +26,7 @@ export default function WeatherIcon (props) {
     return (
     <ReactAnimatedWeather
     icon={codeMapping[props.code]}
-    size= {50}
+    size= {48}
     animate={true}
   />
 );
