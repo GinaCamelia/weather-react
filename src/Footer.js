3336ee42e18e5d6bc34502061fb72193a571a1af
GinaCamelia <92101771+GinaCamelia@users.noreply.github.com>
2022-05-09 15:40:19 +0100

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -12,7 +12,7 @@ export default function Footer() {
               by <a href="https://laughing-kilby-b8cd7c.netlify.app" 
               className="link"
               target="_blank"
-              rel="noreferrer">Gina-Camelia Hodorogea </a>
+              rel="noreferrer">Gina-Camelia Hodorogea </a> and hosted on <a href="https://euphonious-gnome-570ef0.netlify.app" target="_blank" rel="no referrer" className="link">Netlify</a>
             </div>
               <div className="contact-details">
                 <span className="email"><strong>E-mail:{" "}</strong></span>
